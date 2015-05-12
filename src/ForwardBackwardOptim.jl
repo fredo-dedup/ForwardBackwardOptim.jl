f8d0a20943026408c6f2acf0050aa9ab37de80ab
fredo <frederic.testard@gmail.com>
2015-05-12 18:05:14 +0200

    initial commit

--- src/ForwardBackwardOptim.jl
+++ src/ForwardBackwardOptim.jl
@@ -1,5 +1,20 @@
 module ForwardBackwardOptim
 
-# package code goes here
+using Compat
+using Reexport
+@reexport using EmpiricalRisks
+
+import Base.LinAlg: axpy!
+
+# source files
+
+include("callback.jl")
+include("fbs.jl")
+include("fista.jl")
+include("fasta.jl")
+
+export  fbs!, fbs,
+        fista!, fista,
+        fasta!, fasta
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,15 @@
-using ForwardBackwardOptim
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+reload("ForwardBackwardOptim")
+m = ForwardBackwardOptim
+
+tests = [
+    "optims"
+]
+
+for t in tests
+    tfile = string(t, ".jl")
+    println("  * $tfile ...")
+    include(tfile)
+end
+
